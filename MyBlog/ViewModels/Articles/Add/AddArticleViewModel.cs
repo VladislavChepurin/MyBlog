8814d2ad0b92ec076d829b26239b1204db49683f
Vladislav <vlad1988.1@gmail.com>
2023-03-06 20:05:03 +0300

    fix

---
+++ MyBlog/ViewModels/Articles/Add/AddArticleViewModel.cs
@@ -0,0 +1,20 @@
+﻿using MyBlog.Models.Articles;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyBlog.ViewModels.Articles.Add
+{
+    public class AddArticleViewModel
+    {
+        public List<Teg>? Tegs { get; set; }
+
+        [Required(ErrorMessage = "Поле обязательно для заполнения")]
+        [DataType(DataType.Text)]
+        [Display(Name = "Заголовок", Prompt = "Введите заголовок")]
+        public string? Title { get; set; }
+
+        [Required(ErrorMessage = "Поле обязательно для заполнения")]
+        [DataType(DataType.Text)]
+        [Display(Name = "Контент", Prompt = "Введите текст статьи")]
+        public string? Content { get; set; }
+    }
+}
